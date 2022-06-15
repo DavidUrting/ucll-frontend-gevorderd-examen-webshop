3d2e4db8fd7451cdf2d81920bc30d40c0aa2517a
DavidUrting <david.urting@gmail.com>
2022-06-15 20:32:10 +0200

    vereenvoudiging opdracht

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ import products from "./products.js";
 import Cart from "./cart.js";
 import CartItem from "./cart-item.js";
 
+import { v4 as uuidv4 } from "uuid";
+
 import express from "express";
 import cors from "cors";
 const app = express();
@@ -14,65 +16,49 @@ app.use(express.json());
 // **********************
 // Geeft een array van alle producten terug. 
 // Deze array bestaat uit objecten met id, title en cost properties.
+// Dus een array van deze vorm: [{productId: 0, title: "USB stick", cost: 10}, ...]
 app.get("/api/products", (req, res) => {
     res.json(products);
     console.log(`Er werden ${products.length} producten naar de browser teruggestuurd.`);
 });
 
-// In deze array wordt een lijst bijgehouden met alle winkelwagens.
-// Aangezien dit een server toepassing is kunnen er dus winkelwagens in zitten van meerdere gebruikers.
-// Bij wijze van voorbeeld wordt er voor één bekende user al een winkelwagen toegevoegd.
-let usersAndTheirCarts = [new Cart('timbernerslee', [
-    new CartItem(0, 2),
-    new CartItem(2, 5)
-])];
+// HTTP POST /api/cart
+// *******************
+// Berekent de totaalprijs van een winkelwagen met korting.
+// (De totaalprijs zonder korting moet je zelf berekenen in de client.)
+// Deze functie verwacht een JSON body met de inhoud van je winkelwagen.
+// Een winkelwagen kan je doorgeven als volgt:
+// (1) een array van product ID's [0,2, ...]
+// (2) ofwel een array van objecten met een productId property: [{productId: 0}, {productId: 2}]
+// (3) ofwel een array van objecten met productId en amount properties: 
+//     [{productId:0, amount: 10}, {productId:2, amount: 3}]
+// Een product ID mag meermaals voorkomen. Dit mag dus: [0,0,0,0,0,2,2, ...]
+app.post("/api/cart", (req, res) => { 
+    let cart = createCartFromRequest(req.body);
 
-// HTTP GET /api/cart/username
-// ***************************
-// Geeft de winkelwagen van een bepaalde gebruiker terug. 
-// Geeft een object terug met daarin een 'items' property: dat is een array met objecten die 'productId' en 'amount' properties hebben.
-// Het object heeft ook een 'costAfterReduction' property: daarin staat de totale kostprijs met toepassing van het (geheime :) kortingsalgoritme.
-// En verder heeft het ook een 'username' property, waarin de 'username' staat.
-// Indien de gebruiker nog geen winkelwagen heeft wordt een lege winkelwagen teruggestuurd (de 'items' property is een lege array).
-app.get("/api/cart/:username", (req, res) => {
-    let cartOfUser = usersAndTheirCarts.find(c => c.username == req.params.username);
-    if (!cartOfUser) {
-        res.json(new Cart(req.params.username));
-        console.log(`Gebruiker ${req.params.username} heeft nog geen winkelwagen.`);
-    } else {
-        res.json(cartOfUser);
-        console.log(`Gebruiker ${req.params.username} heeft een winkelwagen.`);
-    }    
-}); 
-
-// HTTP POST /api/cart/username
-// ****************************
-// Voegt één artikel van het product toe aan de winkelwagen. In de body moet je een object meegeven met een 'productId' property.
-// Als response wordt hetzelfde teruggegeven als bij een GET naar /api/cart/username.
-app.post("/api/cart/:username", (req, res) => { 
-    let cartOfUser = usersAndTheirCarts.find(c => c.username == req.params.username);
-    if (!cartOfUser) {
-        cartOfUser = new Cart(req.params.username);
-        usersAndTheirCarts.push(cartOfUser); 
+    let objectTotalPriceWithReduction = {
+        totalPriceWithReduction: cart.totalPriceWithReduction
     }
-    cartOfUser.addToCart(req.body.productId);
-    res.json(cartOfUser);
+
+    res.json(objectTotalPriceWithReduction);
 });
 
-// HTTP DELETE /api/cart/username
-// ******************************
-// Verwijdert één artikel van het product uit de winkelwagen. In de body moet je een object meegeven met een 'productId' property.
-// Als het product niet in te winkelwagen voorkomt is dat geen probleem (er zal geen fout worden gegeven).
-// Als response wordt hetzelfde teruggegeven als bij een GET naar /api/cart/username.
-app.delete("/api/cart/:username", (req, res) => {
-    let cartOfUser = usersAndTheirCarts.find(c => c.username == req.params.username);
-    if (!cartOfUser) {
-        res.json(new Cart());
+// HTTP POST /api/order
+// ********************
+// Deze functie plaatst zogezegd de bestelling.
+// In de body wordt een object verwacht met 'username' en 'cartItems' properties:
+// - username: is een string en bevat de naam van de gebruiker.
+// - cartItems: stelt de winkelwagen voor (is een array zoals deze ook verwacht wordt in /api/cart).
+app.post("/api/order", (req, res) => {
+    let username = req.body.username;
+    let cart = createCartFromRequest(req.body.cartItems);
+    let orderId = placeOrder(username, cart);
+
+    let objectWithOrderId = {
+        orderId: orderId
     }
-    else {
-        cartOfUser.removeFromCart(req.body.productId);
-        res.json(cartOfUser);
-    }
+
+    res.json(objectWithOrderId);
 });
 
 
@@ -80,3 +66,38 @@ app.delete("/api/cart/:username", (req, res) => {
 app.listen(3000, () => {
     console.log("Server is listening...");
 });
+
+// Deze interne functie transformeert een array van winkelwagen items
+// (die door de browser werd verstuurd) naar een intern Cart object.
+// Het Cart object wordt voornamelijk gebruikt om de totaalprijs met korting
+// te berekenen.
+function createCartFromRequest(cartItems) {
+    let cart = new Cart([]);
+    for (let i = 0; i < cartItems.length; i++) {
+        let productIdOrObject = cartItems[i];
+        if (typeof productIdOrObject === 'number' || productIdOrObject instanceof Number) {
+            cart.addToCart(productIdOrObject, 1);
+        } else {
+            if (productIdOrObject.amount) {
+                cart.addToCart(
+                    productIdOrObject.productId,
+                    productIdOrObject.amount);
+            } else {
+                cart.addToCart(
+                    productIdOrObject.productId, 
+                    1);            
+            }
+        }
+    }
+    return cart;
+}
+
+// Plaatst het order in de backend systemen ...
+// ... maar dat is out of scope :)
+function placeOrder(username, cart) {
+    let orderId = uuidv4();
+
+    // TODO: order verwerken en in database bewaren...
+
+    return orderId;
+}
--- server/cart.js
+++ server/cart.js
@@ -2,9 +2,9 @@ import products from "./products.js";
 import CartItem from "./cart-item.js";
 
 // Objecten van deze klasse stellen een winkelkar voor.
+// Wordt enkel door de backend gebruikt om de kostprijs inclusief korting te berekenen.
 export default class Cart {
-    constructor(username, cartItems) {
-        this._username = username;
+    constructor(cartItems) {
         if (cartItems) {
             this._items = cartItems;
         } else {
@@ -20,7 +20,7 @@ export default class Cart {
         return this._items;
     }
 
-    get costAfterReduction() {
+    get totalPriceWithReduction() {
         let ourBestPrice = 0;
         this._items.forEach((item) => {
             ourBestPrice += (item.amount * products.find(p => p.id === item.productId).cost);
@@ -31,36 +31,13 @@ export default class Cart {
         return ourBestPrice;
     }
 
-    addToCart(productId) {
+    addToCart(productId, amount) {
         let itemInCart = this._items.find(ci => ci.productId === productId);
         if (!itemInCart) {
-            itemInCart = new CartItem(productId, 1);
+            itemInCart = new CartItem(productId, amount ? amount : 1);
             this._items.push(itemInCart);
         } else {
-            itemInCart.amount++;
+            itemInCart.amount += (amount ? amount : 1);
         }
-    }
-
-    removeFromCart(productId) {
-        let itemInCart = this._items.find(ci => ci.productId === productId);
-        if (!itemInCart) { 
-            return;
-        }
-        else {
-            itemInCart.amount--;
-            if (itemInCart.amount <= 0) {
-                this._items = this._items.filter(ci => ci.productId !== productId);
-            }
-        }
-    }
-
-    // JSON.stringify zal standaard deze methode aanroepen, alvoerens om te zetten naar een JSON string.
-    // Op deze manier zorgen we ervoor dat er geen _ in de propertynamen zitten.
-    toJSON() {
-        return {
-            username: this._username,
-            items: this._items,
-            costAfterReduction: this.costAfterReduction
-        };
     }
 }
